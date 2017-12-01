ec56b5c33aec300097ba221f42a2753a8e1f3e9f
zam121118 <zam121118@163.com>
2017-12-01 23:49:48 +0800

    review main.py tmp.py docker_mbbo.py,删除tmp.py docker_mbbo.py更名main.py为doc_mbbo.py; 下一步1. 判断doc_mbbo.py逻辑是否正确；2. 利用test5测试集进行初步聚合；3. 对doc_mbbo.py引入在vm_mbbo.py中使用的仅对running vms/hms进行代价计算；4. review Arch_Contrast_mbbo.py模块进行mbbo在不同聚合粒度下的模拟测试

--- cmbbo/Arch_Contrast_mbbo.py
+++ cmbbo/Arch_Contrast_mbbo.py
@@ -1,10 +1,13 @@
 # coding: utf-8
-
+'''
+@Date: 2017-12-2
 ## 这个文件主要对比docker-hm与docker-vm-pm的实验场景，在size,generation,初始容器数量与尺寸,完全相同；
 ## 且docker-vm-hm中vm:hm=1:1的情况下进行测试判断，
 ## 测试1： 仅以power_cost为HSI的情景下
 ## 测试2： 以power_cost和 vm balance index为HSI的情景下
 ## 测试3： 考虑到migration_time的HSI比较
+'''
+
 import time
 import random
 import math
